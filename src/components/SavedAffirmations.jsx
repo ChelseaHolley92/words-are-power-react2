a626975a39408c5e24b20d73d12db88832c9d899
Your Username <ChelseaHolley23@gmail.com>
2025-03-12 15:22:56 -0400

    commit

--- src/components/SavedAffirmations.jsx
+++ src/components/SavedAffirmations.jsx
@@ -2,3 +2,12 @@ import React from "react";
 
 const SavedAffirmations = () => {
     const savedAffirmations = JSON.parse(localStorage.getItem("savedAffirmations")) || [];
+
+    return (
+        <div className="saved-affirmations">
+            <h2>Saved Affirmations</h2>
+            {savedAffirmations.length > 0 ? (
+                <ul>
+                    {savedAffirmations.map((afirmation, index)) => (
+                </ul>
+        </div>
