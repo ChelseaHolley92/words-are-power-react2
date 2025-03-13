93b4db417a716e07a5bfa11f6c0c9de68e589051
Your Username <ChelseaHolley23@gmail.com>
2025-03-13 15:27:58 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
                 <Route path="/" element={
                   <>
                 <button onClick={toggleTheme} className="theme-toggle-btn">
-                  {theme === "Default" ? "Dark Mode" : "Light Mode"}
+                  {theme === "dark" ? "Light Mode" : "Dark Mode"}
                 </button>
                 <Header />
                 <NameInput onSave={handleSaveName} />
