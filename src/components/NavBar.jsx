6cf14997c1dd1566fc498b54ec59f39674346d43
Your Username <ChelseaHolley23@gmail.com>
2025-03-13 12:24:11 -0400

    comm

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <h1>Words Are Power</h1>
+           
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/saved">Saved Affirmations</Link></li>
